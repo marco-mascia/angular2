9be4de6aac5f24cee2e5778a53e2547e43ab3394
Marco Mascia <mmascia@board.com>
2019-10-04 10:34:29 +0200

    Added login and cancel buttons behavior

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core'
 import { createOfflineCompileUrlResolver } from '@angular/compiler'
 import { AuthService } from '../auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: './login.component.html'
@@ -8,12 +9,19 @@ import { AuthService } from '../auth.service'
 
 export class LoginComponent {
 
-    constructor(@Inject(AuthService) private authService:AuthService){}
+    constructor(
+      @Inject(AuthService) private authService:AuthService,
+      @Inject(Router) private router:Router
+    ){}
     
     userName
     password
     
     login(formValues){
         this.authService.loginUser(formValues.userName, formValues.password);        
+        this.router.navigate(['events'])
+    }
+    cancel(){
+      this.router.navigate(['events']);
     }
 }
